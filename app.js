5db604fc28ce14735c9bd4fc62034fa6f8549c47
Omar Rizwan <omar.rizwan@gmail.com>
2017-10-05 20:46:21 -0700

    Editor support.

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-/* var myTextarea = document.getElementById("myTextarea");
- * var editor = CodeMirror.fromTextArea(myTextarea, {
- *   lineNumbers: true
- * });*/
-
-var s = `
+var myTextarea = document.getElementById("myTextarea");
+myTextarea.value = `
 # Red and Orange
 
 - Richmond
@@ -80,177 +76,193 @@ var s = `
 - Warm Springs/South Fremont
 `;
 
-var routes = (function(s) {
-  const routes = {};
+var editor = CodeMirror.fromTextArea(myTextarea, {
+  lineNumbers: true
+});
+editor.on("change", () => {
+  render(editor.getValue());
+});
+render(editor.getValue());
 
-  let activeRoutes = [];
-  for (let line of s.split("\n")) {
-    if (line.trim() === "") continue;
+var simulation;
+function render(s) {
+  if (simulation) simulation.stop();
+  document.querySelector("svg").innerHTML = "";
 
-    if (line.startsWith("#")) {
-      activeRoutes = line
-        .substr(1)
-        .trim()
-        .split(/\s*(?:,|\s+and\s+)+\s*/);
-    } else if (line.startsWith("-")) {
-      line = line.substr(1).trim();
-      const terminalMatch = line.match(/\[([^\]]+)\s+terminal\]/);
+  var routes = (function(s) {
+    const routes = {};
 
-      let terminates = [];
-      if (terminalMatch) {
-        terminates = terminalMatch
-          ? terminalMatch[1].split(/\s*(?:,|\s+and\s+)+\s*/)
-          : [];
-        line = line.replace(terminalMatch[0], "").trim();
-      }
-      const stop = {
-        name: line,
-        terminates
-      };
-      for (let route of activeRoutes) {
-        if (!(route in routes)) routes[route] = [];
-        const routeStops = routes[route];
-        if (
-          routeStops.length > 0 &&
-          routeStops[routeStops.length - 1].terminates.includes(route)
-        ) {
-          routes[route].unshift(stop);
-        } else {
-          routes[route].push(stop);
+    let activeRoutes = [];
+    for (let line of s.split("\n")) {
+      if (line.trim() === "") continue;
+
+      if (line.startsWith("#")) {
+        activeRoutes = line
+          .substr(1)
+          .trim()
+          .split(/\s*(?:,|\s+and\s+)+\s*/);
+      } else if (line.startsWith("-")) {
+        line = line.substr(1).trim();
+        const terminalMatch = line.match(/\[([^\]]+)\s+terminal\]/);
+
+        let terminates = [];
+        if (terminalMatch) {
+          terminates = terminalMatch
+            ? terminalMatch[1].split(/\s*(?:,|\s+and\s+)+\s*/)
+            : [];
+          line = line.replace(terminalMatch[0], "").trim();
+        }
+        const stop = {
+          name: line,
+          terminates
+        };
+        for (let route of activeRoutes) {
+          if (!(route in routes)) routes[route] = [];
+          const routeStops = routes[route];
+          if (
+            routeStops.length > 0 &&
+            routeStops[routeStops.length - 1].terminates.includes(route)
+          ) {
+            routes[route].unshift(stop);
+          } else {
+            routes[route].push(stop);
+          }
         }
       }
     }
-  }
-  return routes;
-})(s);
+    return routes;
+  })(s);
 
-console.log(routes);
+  console.log(routes);
 
-var graph = {
-  nodes: [...new Set([].concat.apply([], Object.values(routes)))].map(stop => ({
-    id: stop.name,
-    group: 1
-  })),
-  links: [].concat.apply(
-    [],
-    Object.values(routes).map(function(route) {
-      const routeLinks = [];
-      for (let i = 0; i < route.length; i++) {
-        if (!route[i + 1]) continue;
-        routeLinks.push({
-          source: route[i].name,
-          target: route[i + 1].name,
-          value: 1
-        });
-      }
-      return routeLinks;
-    })
-  )
-};
-console.log(graph);
+  var graph = {
+    nodes: [
+      ...new Set([].concat.apply([], Object.values(routes)))
+    ].map(stop => ({
+      id: stop.name,
+      group: 1
+    })),
+    links: [].concat.apply(
+      [],
+      Object.values(routes).map(function(route) {
+        const routeLinks = [];
+        for (let i = 0; i < route.length; i++) {
+          if (!route[i + 1]) continue;
+          routeLinks.push({
+            source: route[i].name,
+            target: route[i + 1].name,
+            value: 1
+          });
+        }
+        return routeLinks;
+      })
+    )
+  };
+  console.log(graph);
 
-var svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+  var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-var simulation = d3
-  .forceSimulation()
-  .force(
-    "link",
-    d3.forceLink().id(function(d) {
+  simulation = d3
+    .forceSimulation()
+    .force(
+      "link",
+      d3.forceLink().id(function(d) {
+        return d.id;
+      })
+    )
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  var link = svg
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("stroke-width", function(d) {
+      return Math.sqrt(d.value);
+    });
+
+  var node = svg
+    .append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter()
+    .append("g")
+    .attr("class", "node")
+    .call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+
+  node
+    .append("circle")
+    .attr("r", 5)
+    .attr("fill", function(d) {
+      return color(d.group);
+    });
+
+  node
+    .append("text")
+    .attr("dx", 12)
+    .attr("dy", ".35em")
+    .text(function(d) {
       return d.id;
-    })
-  )
-  .force("charge", d3.forceManyBody())
-  .force("center", d3.forceCenter(width / 2, height / 2));
+    });
 
-var link = svg
-  .append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(graph.links)
-  .enter()
-  .append("line")
-  .attr("stroke-width", function(d) {
-    return Math.sqrt(d.value);
-  });
-
-var node = svg
-  .append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
-  .data(graph.nodes)
-  .enter()
-  .append("g")
-  .attr("class", "node")
-  .call(
-    d3
-      .drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended)
-  );
-
-node
-  .append("circle")
-  .attr("r", 5)
-  .attr("fill", function(d) {
-    return color(d.group);
-  });
-
-node
-  .append("text")
-  .attr("dx", 12)
-  .attr("dy", ".35em")
-  .text(function(d) {
+  node.append("title").text(function(d) {
     return d.id;
   });
 
-node.append("title").text(function(d) {
-  return d.id;
-});
+  simulation.nodes(graph.nodes).on("tick", ticked);
 
-simulation.nodes(graph.nodes).on("tick", ticked);
+  simulation.force("link").links(graph.links);
 
-simulation.force("link").links(graph.links);
+  function ticked() {
+    link
+      .attr("x1", function(d) {
+        return d.source.x;
+      })
+      .attr("y1", function(d) {
+        return d.source.y;
+      })
+      .attr("x2", function(d) {
+        return d.target.x;
+      })
+      .attr("y2", function(d) {
+        return d.target.y;
+      });
 
-function ticked() {
-  link
-    .attr("x1", function(d) {
-      return d.source.x;
-    })
-    .attr("y1", function(d) {
-      return d.source.y;
-    })
-    .attr("x2", function(d) {
-      return d.target.x;
-    })
-    .attr("y2", function(d) {
-      return d.target.y;
+    node.attr("transform", function(d) {
+      d.x = Math.max(0, Math.min(width, d.x));
+      d.y = Math.max(0, Math.min(height, d.y));
+      return "translate(" + d.x + "," + d.y + ")";
     });
+  }
 
-  node.attr("transform", function(d) {
-    d.x = Math.max(0, Math.min(width, d.x));
-    d.y = Math.max(0, Math.min(height, d.y));
-    return "translate(" + d.x + "," + d.y + ")";
-  });
-}
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
 
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
-}
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
 
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
 }
